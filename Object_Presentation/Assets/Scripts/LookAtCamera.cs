a7438421dbc48b325cae867e9e42969085dd1e68
JensEbert <jens.ebert@bibek.de>
2017-01-23 16:08:03 +0100

    Project Import
    Added the project and unity files.

---
+++ Object_Presentation/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+	public GameObject go_Camera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (go_Camera.transform.position);
+	}
+}
---
+++ Object_Presentation/Assets/Scripts/SubmitNewCameraPosition.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubmitNewCameraPosition : MonoBehaviour 
+{
+	public float f_newCameraX, f_newCameraY, f_newCameraZ;
+	private GameObject go_newTarget;
+	private GameObject go_newTargetChild;
+	public LookAtTarget s_lookAtTargetScript;
+
+	// Use this for initialization
+	void Start ()
+	{
+		go_newTarget = gameObject;
+		go_newTargetChild = gameObject.transform.GetChild (0).gameObject;
+	}
+
+	//When object is clicked, zoom in on it
+	/*void OnMouseDown()
+	{
+		SetNewTarget ();
+	}*/
+
+	public void SetNewTarget ()
+	{
+		s_lookAtTargetScript.LookAtNewTarget(go_newTarget, go_newTargetChild, f_newCameraX, f_newCameraY, f_newCameraZ);
+	}
+}
